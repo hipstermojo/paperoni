9f56c58dd93c738d9afc33ffcf9572b929672c0c
Kenneth Gitere <gitere81@gmail.com>
2020-05-16 10:09:44 +0300

    Add simple CLI wrapper

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,35 @@ use std::fs::File;
 
 use async_std::{fs::create_dir, fs::remove_dir_all, task};
 use epub_builder::{EpubBuilder, EpubContent, ZipLibrary};
+use structopt::StructOpt;
 use url::Url;
 
+mod cli;
 mod extractor;
 
 use extractor::Extractor;
 fn main() {
+    let opt = cli::Opts::from_args();
+    if let Some(url) = opt.url {
+        println!("Downloading single article");
+        download(url)
+    }
+}
+
+async fn fetch_url(url: &str) -> String {
+    let client = surf::Client::new();
+    println!("Fetching...");
+    // TODO: Add middleware for following redirects
+    client
+        .get(url)
+        .recv_string()
+        .await
+        .expect("Unable to fetch URL")
+}
+
+fn download(url: String) {
     task::block_on(async {
-        let urls = vec![
-            "https://saveandrun.com/posts/2020-01-24-generating-mazes-with-haskell-part-1.html",
-            "https://saveandrun.com/posts/2020-04-05-querying-pacman-with-datalog.html",
-            "https://blog.hipstermojo.xyz/posts/redis-orm-preface/",
-            "https://vuejsdevelopers.com/2020/03/31/vue-js-form-composition-api/?utm_campaign=xl5&utm_medium=article&utm_source=vuejsnews#adding-validators",
-            "https://medium.com/typeforms-engineering-blog/the-beginners-guide-to-oauth-dancing-4b8f3666de10",
-            "https://dev.to/steelwolf180/full-stack-development-in-django-3768"
-        ];
-        let html = fetch_url(urls[4]).await;
+        let html = fetch_url(&url).await;
         let mut extractor = Extractor::from_html(&html);
         println!("Extracting");
         extractor.extract_content();
@@ -25,7 +38,7 @@ fn main() {
             .await
             .expect("Unable to create res/ output folder");
         extractor
-            .download_images(&Url::parse(urls[5]).unwrap())
+            .download_images(&Url::parse(&url).unwrap())
             .await
             .expect("Unable to download images");
         let mut out_file = File::create("out.epub").unwrap();
@@ -50,15 +63,4 @@ fn main() {
         println!("Cleaning up");
         remove_dir_all("res/").await.unwrap();
     })
-}
-
-async fn fetch_url(url: &str) -> String {
-    let client = surf::Client::new();
-    println!("Fetching...");
-    // TODO: Add middleware for following redirects
-    client
-        .get(url)
-        .recv_string()
-        .await
-        .expect("Unable to fetch URL")
 }
