703de7e3bfd79cdf3a0c74908cc48666a114cd3b
Kenneth Gitere <gitere81@gmail.com>
2020-10-22 12:12:30 +0300

    Merge the readability module with the rest of the extractor

--- src/main.rs
+++ src/main.rs
@@ -35,36 +35,37 @@ async fn fetch_url(url: &str) -> String {
 fn download(url: String) {
     task::block_on(async {
         let html = fetch_url(&url).await;
-        let mut extractor = Extractor::from_html(&html);
         println!("Extracting");
-        extractor.extract_content();
-        create_dir("res/")
-            .await
-            .expect("Unable to create res/ output folder");
-        extractor
-            .download_images(&Url::parse(&url).unwrap())
-            .await
-            .expect("Unable to download images");
-        let mut out_file = File::create("out.epub").unwrap();
-        let mut html_buf = Vec::new();
-        extractor
-            .content
-            .unwrap()
-            .as_node()
-            .serialize(&mut html_buf)
-            .expect("Unable to serialize");
-        let html_buf = std::str::from_utf8(&html_buf).unwrap();
-        let mut epub = EpubBuilder::new(ZipLibrary::new().unwrap()).unwrap();
-        epub.add_content(EpubContent::new("code.xhtml", html_buf.as_bytes()))
-            .unwrap();
-        for img in extractor.img_urls {
-            let file_path = format!("{}", &img.0);
+        let mut extractor = Extractor::from_html(&html);
+        extractor.extract_content(&url);
+        if extractor.article().is_some() {
+            create_dir("res/")
+                .await
+                .expect("Unable to create res/ output folder");
+            extractor
+                .download_images(&Url::parse(&url).unwrap())
+                .await
+                .expect("Unable to download images");
+            let mut out_file = File::create("out.epub").unwrap();
+            let mut html_buf = Vec::new();
+            extractor
+                .article()
+                .unwrap()
+                .serialize(&mut html_buf)
+                .expect("Unable to serialize");
+            let html_buf = std::str::from_utf8(&html_buf).unwrap();
+            let mut epub = EpubBuilder::new(ZipLibrary::new().unwrap()).unwrap();
+            epub.add_content(EpubContent::new("code.xhtml", html_buf.as_bytes()))
+                .unwrap();
+            for img in extractor.img_urls {
+                let file_path = format!("{}", &img.0);
 
-            let img_buf = File::open(file_path).expect("Can't read file");
-            epub.add_resource(img.0, img_buf, img.1.unwrap()).unwrap();
+                let img_buf = File::open(file_path).expect("Can't read file");
+                epub.add_resource(img.0, img_buf, img.1.unwrap()).unwrap();
+            }
+            epub.generate(&mut out_file).unwrap();
+            println!("Cleaning up");
+            remove_dir_all("res/").await.unwrap();
         }
-        epub.generate(&mut out_file).unwrap();
-        println!("Cleaning up");
-        remove_dir_all("res/").await.unwrap();
     })
 }
